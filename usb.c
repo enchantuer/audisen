aea95fed1ec17813a6d3b53ad5a8423038297c9c
Pénom Nom <killian.guichet@isen-ouest.yncrea.fr>
2024-03-25 18:55:30 +0100

    usb support

--- usb.c
+++ usb.c
@@ -11,15 +11,37 @@
 FT_HANDLE initUSB(){
     FT_HANDLE ftHandle;
 
+    FT_STATUS ftStatus = FT_Open(0, &ftHandle);
+
+    if (ftStatus != FT_OK) {
+        printf("Error: FT_Open(%d) failed\n", 0);
+        return 0;
+    }
+
     return ftHandle;
 
 }
 void closeUSB(FT_HANDLE ftHandle){
-
+    FT_STATUS ftStatus = FT_Close(ftHandle);
+    if (ftStatus == FT_OK) {
+        printf("USB Closed !");
+    }
+    else {
+        printf("USB Error !");
+    }
 }
 
 
 void writeUSB(char* frame, FT_HANDLE ftHandle){
+    DWORD BytesWritten;
+    char TxBuffer[256]; // Contains data to write to device
+    strcpy(TxBuffer, frame);
 
-
+    FT_STATUS ftStatus = FT_Write(ftHandle, TxBuffer, sizeof(TxBuffer), &BytesWritten);
+    if (ftStatus == FT_OK) {
+        printf("USB Work !");
+    }
+    else {
+        printf("USB Error !");
+    }
 }
